5803354fc4d3c42c26e61bc5ccb01e0eec123f95
logongas <lorenzo.profesor@gmail.com>
2015-04-27 13:47:11 +0200

    Incluir la url de la inscripción en el CEFIRE

--- views/main/main.js
+++ views/main/main.js
@@ -1,11 +1,5 @@
 app.controller('MainController', ['$scope', 'goPage', function ($scope, goPage) {
-        $scope.login = function () {
-            goPage.login();
-        };
-
-        $scope.createAccount = function() {
-            goPage.createAccount();  
-        };        
+       
 
     }]);
 
