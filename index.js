eaaa3e5f06a08b6d7cbb49971a6ca03ae25f3eb0
logongas <lorenzo.profesor@gmail.com>
2015-05-01 00:22:28 +0200

    cambiar la estetica inicial

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ var app = angular.module('app', []);
 
 app.controller("IndexController", ['$scope', 'animateScroll', function ($scope, animateScroll) {
         $scope.animateScroll = animateScroll;
+        
+
+        
+
+        
 }]);
