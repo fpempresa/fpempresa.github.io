89399068db79816cc30de3b40a04ab36fcc2c744
Lorenzo Gonzalez <logongas@gmailcom>
2016-05-26 08:00:48 +0200

    Script para instalar Jekyll y permisos de ejecución

---
+++ install-jekyll.sh
@@ -0,0 +1,3 @@
+sudo apt-get install ruby ruby-dev make gcc nodejs
+sudo gem install jekyll --no-rdoc --no-ri
+sudo gem install github-pages --no-rdoc --no-ri
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+#sudo apt-get install ruby ruby-dev make gcc nodejs
+#sudo gem install jekyll --no-rdoc --no-ri
+#sudo gem install github-pages --no-rdoc --no-ri
 bundle exec jekyll serve
