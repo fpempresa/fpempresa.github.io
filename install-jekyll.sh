de37359ae66d551deacf419e97da18ff0a5873b8
Lorenzo Gonzalez <logongas@gmailcom>
2016-05-27 09:10:37 +0200

    fix:Script de instalación de Jekyll

--- install-jekyll.sh
+++ install-jekyll.sh
@@ -1,3 +1,7 @@
-sudo apt-get install ruby ruby-dev make gcc nodejs
+sudo apt-get install  make gcc nodejs software-properties-common 
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby2.2 ruby2.2-dev
+sudo gem install bundler
 sudo gem install jekyll --no-rdoc --no-ri
 sudo gem install github-pages --no-rdoc --no-ri
